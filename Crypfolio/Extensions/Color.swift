516eedb4354b3a3d2e8c6018675bd8be203a01d5
Mauricio Argumedo <mauargudev@gmail.com>
2024-01-28 11:53:51 -0600

    Assets and Color theme implemented

---
+++ Crypfolio/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  Color.swift
+//  Crypfolio
+//
+//  Created by Mauricio Argumedo on 28/1/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
