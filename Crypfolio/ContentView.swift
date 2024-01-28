30bee7a3d7f6792230cd23b69a160490bb25b92f
Mauricio Argumedo <mauargudev@gmail.com>
2024-01-28 11:18:13 -0600

    Initial Commit

---
+++ Crypfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypfolio
+//
+//  Created by Mauricio Argumedo on 28/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypfolio/CrypfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypfolioApp.swift
+//  Crypfolio
+//
+//  Created by Mauricio Argumedo on 28/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CrypfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
